56a41390ae04cf41d63058621022516d019aeef0
Randy Schneck <i@rasch.co>
2022-11-08 02:10:26 -0500

    💄 Update color palettes

--- palettes/css-named-colors.js
+++ palettes/css-named-colors.js
@@ -2,566 +2,705 @@ export const colors = [
   {
     name: "black",
     hex: "#000000",
+    groups: ["gray"],
   },
   {
     name: "silver",
     hex: "#c0c0c0",
+    groups: ["gray"],
   },
   {
     name: "gray",
     hex: "#808080",
     aliases: ["grey"],
+    groups: ["gray"],
   },
   {
     name: "white",
     hex: "#ffffff",
+    groups: ["white"],
   },
   {
     name: "maroon",
     hex: "#800000",
+    groups: ["brown"],
   },
   {
     name: "red",
     hex: "#ff0000",
+    groups: ["red"],
   },
   {
     name: "purple",
     hex: "#800080",
+    groups: ["violet"],
   },
   {
     name: "fuchsia",
     hex: "#ff00ff",
     aliases: ["magenta"],
+    groups: ["violet"],
   },
   {
     name: "green",
     hex: "#008000",
+    groups: ["green"],
   },
   {
     name: "lime",
     hex: "#00ff00",
+    groups: ["green"],
   },
   {
     name: "olive",
     hex: "#808000",
+    groups: ["green"],
   },
   {
     name: "yellow",
     hex: "#ffff00",
+    groups: ["yellow"],
   },
   {
     name: "navy",
     hex: "#000080",
+    groups: ["blue"],
   },
   {
     name: "blue",
     hex: "#0000ff",
+    groups: ["blue"],
   },
   {
     name: "teal",
     hex: "#008080",
+    groups: ["cyan"],
   },
   {
     name: "aqua",
     hex: "#00ffff",
     aliases: ["cyan"],
+    groups: ["cyan"],
   },
   {
     name: "orange",
     hex: "#ffa500",
+    groups: ["orange"],
   },
   {
     name: "aliceblue",
     hex: "#f0f8ff",
+    groups: ["white"],
   },
   {
     name: "antiquewhite",
     hex: "#faebd7",
+    groups: ["white"],
   },
   {
     name: "aquamarine",
     hex: "#7fffd4",
+    groups: ["cyan"],
   },
   {
     name: "azure",
     hex: "#f0ffff",
+    groups: ["white"],
   },
   {
     name: "beige",
     hex: "#f5f5dc",
+    groups: ["white"],
   },
   {
     name: "bisque",
     hex: "#ffe4c4",
+    groups: ["brown"],
   },
   {
     name: "blanchedalmond",
     hex: "#ffebcd",
+    groups: ["brown"],
   },
   {
     name: "blueviolet",
     hex: "#8a2be2",
+    groups: ["violet"],
   },
   {
     name: "brown",
     hex: "#a52a2a",
+    groups: ["brown"],
   },
   {
     name: "burlywood",
     hex: "#deb887",
+    groups: ["brown"],
   },
   {
     name: "cadetblue",
     hex: "#5f9ea0",
+    groups: ["cyan"],
   },
   {
     name: "chartreuse",
     hex: "#7fff00",
+    groups: ["green"],
   },
   {
     name: "chocolate",
     hex: "#d2691e",
+    groups: ["brown"],
   },
   {
     name: "coral",
     hex: "#ff7f50",
+    groups: ["orange"],
   },
   {
     name: "cornflowerblue",
     hex: "#6495ed",
+    groups: ["blue"],
   },
   {
     name: "cornsilk",
     hex: "#fff8dc",
+    groups: ["brown"],
   },
   {
     name: "crimson",
     hex: "#dc143c",
+    groups: ["red"],
   },
   {
     name: "darkblue",
     hex: "#00008b",
+    groups: ["blue"],
   },
   {
     name: "darkcyan",
     hex: "#008b8b",
+    groups: ["cyan"],
   },
   {
     name: "darkgoldenrod",
     hex: "#b8860b",
+    groups: ["brown"],
   },
   {
     name: "darkgray",
     hex: "#a9a9a9",
     aliases: ["darkgrey"],
+    groups: ["gray"],
   },
   {
     name: "darkgreen",
     hex: "#006400",
+    groups: ["green"],
   },
   {
     name: "darkkhaki",
     hex: "#bdb76b",
+    groups: ["yellow"],
   },
   {
     name: "darkmagenta",
     hex: "#8b008b",
+    groups: ["violet"],
   },
   {
     name: "darkolivegreen",
     hex: "#556b2f",
+    groups: ["green"],
   },
   {
     name: "darkorange",
     hex: "#ff8c00",
+    groups: ["orange"],
   },
   {
     name: "darkorchid",
     hex: "#9932cc",
+    groups: ["violet"],
   },
   {
     name: "darkred",
     hex: "#8b0000",
+    groups: ["red"],
   },
   {
     name: "darksalmon",
     hex: "#e9967a",
+    groups: ["red"],
   },
   {
     name: "darkseagreen",
     hex: "#8fbc8f",
+    groups: ["green"],
   },
   {
     name: "darkslateblue",
     hex: "#483d8b",
+    groups: ["violet"],
   },
   {
     name: "darkslategray",
     hex: "#2f4f4f",
     aliases: ["darkslategrey"],
+    groups: ["gray"],
   },
   {
     name: "darkturquoise",
     hex: "#00ced1",
+    groups: ["cyan"],
   },
   {
     name: "darkviolet",
     hex: "#9400d3",
+    groups: ["violet"],
   },
   {
     name: "deeppink",
     hex: "#ff1493",
+    groups: ["pink"],
   },
   {
     name: "deepskyblue",
     hex: "#00bfff",
+    groups: ["blue"],
   },
   {
     name: "dimgray",
     hex: "#696969",
     aliases: ["dimgrey"],
+    groups: ["gray"],
   },
   {
     name: "dodgerblue",
     hex: "#1e90ff",
+    groups: ["blue"],
   },
   {
     name: "firebrick",
     hex: "#b22222",
+    groups: ["red"],
   },
   {
     name: "floralwhite",
     hex: "#fffaf0",
+    groups: ["white"],
   },
   {
     name: "forestgreen",
     hex: "#228b22",
+    groups: ["green"],
   },
   {
     name: "gainsboro",
     hex: "#dcdcdc",
+    groups: ["gray"],
   },
   {
     name: "ghostwhite",
     hex: "#f8f8ff",
+    groups: ["white"],
   },
   {
     name: "gold",
     hex: "#ffd700",
+    groups: ["yellow"],
   },
   {
     name: "goldenrod",
     hex: "#daa520",
+    groups: ["brown"],
   },
   {
     name: "greenyellow",
     hex: "#adff2f",
+    groups: ["green"],
   },
   {
     name: "honeydew",
     hex: "#f0fff0",
+    groups: ["white"],
   },
   {
     name: "hotpink",
     hex: "#ff69b4",
+    groups: ["pink"],
   },
   {
     name: "indianred",
     hex: "#cd5c5c",
+    groups: ["red"],
   },
   {
     name: "indigo",
     hex: "#4b0082",
+    groups: ["violet"],
   },
   {
     name: "ivory",
     hex: "#fffff0",
+    groups: ["white"],
   },
   {
     name: "khaki",
     hex: "#f0e68c",
+    groups: ["yellow"],
   },
   {
     name: "lavender",
     hex: "#e6e6fa",
+    groups: ["violet"],
   },
   {
     name: "lavenderblush",
     hex: "#fff0f5",
+    groups: ["white"],
   },
   {
     name: "lawngreen",
     hex: "#7cfc00",
+    groups: ["green"],
   },
   {
     name: "lemonchiffon",
     hex: "#fffacd",
+    groups: ["yellow"],
   },
   {
     name: "lightblue",
     hex: "#add8e6",
+    groups: ["blue"],
   },
   {
     name: "lightcoral",
     hex: "#f08080",
+    groups: ["red"],
   },
   {
     name: "lightcyan",
     hex: "#e0ffff",
+    groups: ["cyan"],
   },
   {
     name: "lightgoldenrodyellow",
     hex: "#fafad2",
+    groups: ["yellow"],
   },
   {
     name: "lightgray",
     hex: "#d3d3d3",
     aliases: ["lightgrey"],
+    groups: ["gray"],
   },
   {
     name: "lightgreen",
     hex: "#90ee90",
+    groups: ["green"],
   },
   {
     name: "lightpink",
     hex: "#ffb6c1",
+    groups: ["pink"],
   },
   {
     name: "lightsalmon",
     hex: "#ffa07a",
+    groups: ["red"],
   },
   {
     name: "lightseagreen",
     hex: "#20b2aa",
+    groups: ["cyan"],
   },
   {
     name: "lightskyblue",
     hex: "#87cefa",
+    groups: ["blue"],
   },
   {
     name: "lightslategray",
     hex: "#778899",
     aliases: ["lightslategrey"],
+    groups: ["gray"],
   },
   {
     name: "lightsteelblue",
     hex: "#b0c4de",
+    groups: ["blue"],
   },
   {
     name: "lightyellow",
     hex: "#ffffe0",
+    groups: ["yellow"],
   },
   {
     name: "limegreen",
     hex: "#32cd32",
+    groups: ["green"],
   },
   {
     name: "linen",
     hex: "#faf0e6",
+    groups: ["white"],
   },
   {
     name: "mediumaquamarine",
     hex: "#66cdaa",
+    groups: ["green"],
   },
   {
     name: "mediumblue",
     hex: "#0000cd",
+    groups: ["blue"],
   },
   {
     name: "mediumorchid",
     hex: "#ba55d3",
+    groups: ["violet"],
   },
   {
     name: "mediumpurple",
     hex: "#9370db",
+    groups: ["violet"],
   },
   {
     name: "mediumseagreen",
     hex: "#3cb371",
+    groups: ["green"],
   },
   {
     name: "mediumslateblue",
     hex: "#7b68ee",
+    groups: ["violet"],
   },
   {
     name: "mediumspringgreen",
     hex: "#00fa9a",
+    groups: ["green"],
   },
   {
     name: "mediumturquoise",
     hex: "#48d1cc",
+    groups: ["cyan"],
   },
   {
     name: "mediumvioletred",
     hex: "#c71585",
+    groups: ["pink"],
   },
   {
     name: "midnightblue",
     hex: "#191970",
+    groups: ["blue"],
   },
   {
     name: "mintcream",
     hex: "#f5fffa",
+    groups: ["white"],
   },
   {
     name: "mistyrose",
     hex: "#ffe4e1",
+    groups: ["white"],
   },
   {
     name: "moccasin",
     hex: "#ffe4b5",
+    groups: ["yellow"],
   },
   {
     name: "navajowhite",
     hex: "#ffdead",
+    groups: ["brown"],
   },
   {
     name: "oldlace",
     hex: "#fdf5e6",
+    groups: ["white"],
   },
   {
     name: "olivedrab",
     hex: "#6b8e23",
+    groups: ["green"],
   },
   {
     name: "orangered",
     hex: "#ff4500",
+    groups: ["orange"],
   },
   {
     name: "orchid",
     hex: "#da70d6",
+    groups: ["violet"],
   },
   {
     name: "palegoldenrod",
     hex: "#eee8aa",
+    groups: ["yellow"],
   },
   {
     name: "palegreen",
     hex: "#98fb98",
+    groups: ["green"],
   },
   {
     name: "paleturquoise",
     hex: "#afeeee",
+    groups: ["cyan"],
   },
   {
     name: "palevioletred",
     hex: "#db7093",
+    groups: ["pink"],
   },
   {
     name: "papayawhip",
     hex: "#ffefd5",
+    groups: ["yellow"],
   },
   {
     name: "peachpuff",
     hex: "#ffdab9",
+    groups: ["yellow"],
   },
   {
     name: "peru",
     hex: "#cd853f",
+    groups: ["brown"],
   },
   {
     name: "pink",
     hex: "#ffc0cb",
+    groups: ["pink"],
   },
   {
     name: "plum",
     hex: "#dda0dd",
+    groups: ["violet"],
   },
   {
     name: "powderblue",
     hex: "#b0e0e6",
+    groups: ["blue"],
   },
   {
     name: "rosybrown",
     hex: "#bc8f8f",
+    groups: ["brown"],
   },
   {
     name: "royalblue",
     hex: "#4169e1",
+    groups: ["blue"],
   },
   {
     name: "saddlebrown",
     hex: "#8b4513",
+    groups: ["brown"],
   },
   {
     name: "salmon",
     hex: "#fa8072",
+    groups: ["red"],
   },
   {
     name: "sandybrown",
     hex: "#f4a460",
+    groups: ["brown"],
   },
   {
     name: "seagreen",
     hex: "#2e8b57",
+    groups: ["green"],
   },
   {
     name: "seashell",
     hex: "#fff5ee",
+    groups: ["white"],
   },
   {
     name: "sienna",
     hex: "#a0522d",
+    groups: ["brown"],
   },
   {
     name: "skyblue",
     hex: "#87ceeb",
+    groups: ["blue"],
   },
   {
     name: "slateblue",
     hex: "#6a5acd",
+    groups: ["violet"],
   },
   {
     name: "slategray",
     hex: "#708090",
     aliases: ["slategrey"],
+    groups: ["gray"],
   },
   {
     name: "snow",
     hex: "#fffafa",
+    groups: ["white"],
   },
   {
     name: "springgreen",
     hex: "#00ff7f",
+    groups: ["green"],
   },
   {
     name: "steelblue",
     hex: "#4682b4",
+    groups: ["blue"],
   },
   {
     name: "tan",
     hex: "#d2b48c",
+    groups: ["brown"],
   },
   {
     name: "thistle",
     hex: "#d8bfd8",
+    groups: ["violet"],
   },
   {
     name: "tomato",
     hex: "#ff6347",
+    groups: ["orange"],
   },
   {
     name: "turquoise",
     hex: "#40e0d0",
+    groups: ["cyan"],
   },
   {
     name: "violet",
     hex: "#ee82ee",
+    groups: ["violet"],
   },
   {
     name: "wheat",
     hex: "#f5deb3",
+    groups: ["brown"],
   },
   {
     name: "whitesmoke",
     hex: "#f5f5f5",
+    groups: ["white"],
   },
   {
     name: "yellowgreen",
     hex: "#9acd32",
+    groups: ["green"],
   },
   {
     name: "rebeccapurple",
     hex: "#663399",
+    groups: ["violet"],
   },
 ]
