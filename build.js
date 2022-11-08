b1ba9c9e4dea4f75466d83bebfa9ba7d7ab2cfa7
Randy Schneck <i@rasch.co>
2022-11-08 06:47:43 -0500

    🚸 Add color aliases to CSS build

--- build.js
+++ build.js
@@ -1,5 +1,12 @@
 import(`./${process.argv[2]}`).then(module => {
-  const customProps = module.colors.map(c => `--${c.name}:${c.hex};`).join("")
+  const customProps = module.colors.map(c => {
+    const aliases = c.aliases
+      ? c.aliases.map(a => `--${a}:${c.hex};`).join("")
+      : ""
+
+    return `--${c.name}:${c.hex};${aliases}`
+  }).join("")
+
   const css = `:where(html){${customProps}}`
 
   console.log(css)
