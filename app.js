6f3265f750fd917ea6e0f10550d5eb1ed1f51a85
Randy Schneck <i@rasch.co>
2022-11-16 00:04:30 -0500

    ✨ Add modal/dialog box for color details
    - Add modal to display color aliases and groups
    - Refactor and clean up code a bit
    - Work on a11y (semantics and keyboard navigation)

--- app.js
+++ app.js
@@ -33,39 +33,118 @@ const parse = colors =>
 const orderBy = prop => (a, b) =>
   a[prop] < b[prop] ? -1 : a[prop] > b[prop] ? 1 : 0
 
-// display :: ([Color], String) -> String
+// swatchColor :: Color -> String
 // NOTE: a threshold of 112 gives fairly reasonable results to minimize
 // the number of swatches that don't meet WCAG color contrast standards.
+const swatchColor = color => `
+  background-color:${color.hex};
+  color:${brightness(hex2rgb(color.hex)) > 112 ? "#000" : "#fff"};
+`.trim()
+
+// display :: ([Color], String) -> String
 const display = (colors, sort) =>
-  parse(colors).sort(orderBy(sort)).map(color => `
-    <div style="background-color:${color.hex}" class="color-card">
-      <p style="color:${brightness(color) > 112 ? "#000" : "#fff"}">
-        ${color.name}<br><span class="hex">${color.hex}</span>
-      </p>
-    </div>
-  `).join("")
+  parse(colors)
+  .sort(orderBy(sort))
+  .map(color => `
+    <li
+      class="card"
+      data-hex="${color.hex}"
+      style="${swatchColor(color)}"
+      tabindex="0"
+    >
+      <p data-hex="${color.hex}" class="name">${color.name}</p>
+      <p data-hex="${color.hex}" class="hex">${color.hex}</p>
+    </li>`.trim())
+  .join("")
+
+// colorDialog :: ([Color], Color) -> IO
+const colorDialog = (colors, color) => {
+  const dialog = document.body.querySelector("dialog")
+
+  if (typeof dialog.showModal !== "function")
+    return
+
+  dialog.innerHTML = `
+    <h1 style="${swatchColor(color)}">${color.name}</h1>
+    <p class="hex-color">${color.hex}</p>
+    <div
+      aria-label="close"
+      class="close-button"
+      style="${swatchColor(color)}"
+      tabindex="0"
+      title="close"
+    ></div>
+    ${color.aliases
+      ? `<h2>Aliases</h2>
+         <ul class="aliases">
+           ${color.aliases.map(a => `<li>${a}</li>`).join("")}
+         </ul>`
+      : ""}
+    ${color.groups
+      ? `<h2>Groups</h2>
+         ${color.groups.map(g => `
+         <h3>${g}</h3>
+         <ul class="group">
+           ${colors
+             .filter(c => c.groups?.find(h => h === g))
+             .map(s => `
+               <li
+                 class="swatch"
+                 data-hex="${s.hex}"
+                 style="background-color:${s.hex}"
+                 tabindex="0"
+                 title="${s.name}"
+               ></li>`)
+             .join("")}
+         </ul>`).join("")}`
+      : ""}`
+
+  document.body.style.overflow = "hidden"
+
+  dialog.addEventListener("close", () => {
+    document.body.style.overflow = "auto"
+  })
+
+  document.querySelector(".close-button").addEventListener("click", () => {
+    dialog.close()
+  })
+
+  dialog.showModal()
+}
 
 // options :: { palette: String, sort: String }
 const options = (queryString =>
   queryString.slice(1).split("&").reduce((opts, opt) => ({
     ...opts,
     [opt.split("=")[0]]: decodeURIComponent(opt.split("=")[1]),
-  }), { palette: "the-new-defaults", sort: "brightness" })
+  }), { palette: "crayola-crayons", sort: "brightness" })
 )(window.location.search)
 
 import(`./palettes/${options.palette}.js`)
   .then(module => {
     const app = document.getElementById("app")
+
     app.innerHTML = display(module.colors, options.sort)
+
+    document.addEventListener("click", e => {
+      const hex = e.target.getAttribute("data-hex")
+
+      if (hex) {
+        const color = module.colors.find(c => c.hex === hex)
+
+        colorDialog(module.colors, color)
+        document.activeElement.blur()
+      }
+    })
   })
 
 const palettes = [
+  "crayola-crayons",
   "the-new-defaults",
   "chartpak-ad-markers",
   "prismacolor-markers",
   "prismacolor-pastels",
   "prismacolor-pencils",
-  "crayola-crayons",
   "css-named-colors",
 ]
 
@@ -86,9 +165,12 @@ const keybindings = {
     : palettes.indexOf(options.palette) - 1,
 }
 
-document.addEventListener("keypress", e => {
-  if (keybindings[e.key])
+document.addEventListener("keydown", e => {
+  if (e.key === "Enter") {
+    document.activeElement.click()
+  } else if (keybindings[e.key]) {
     window.location.search = (/^p$/i).test(e.key)
       ? `?sort=${options.sort}&palette=${palettes[keybindings[e.key]()]}`
       : `?sort=${keybindings[e.key]}&palette=${options.palette}`
+  }
 })
